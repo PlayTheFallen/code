5615799ecd82fa193b1e67f0e3ff8e069fd2ee1a
Fallen <PlayTheFallen@gmail.com>
2018-11-06 23:00:32 +0000

    Create edit-array-element.js

---
+++ javascript/Libraries/rethinkdb/edit-array-element.js
@@ -0,0 +1,10 @@
+# Edit array element
+
+This will target a given element within an array and then merge a new object with the existing target.
+
+```js
+// not recommended, used only to target a random post within the table
+r.table('posts').filter({author: 1}).limit(1).update({
+  posts: r.row('posts').changeAt(0, r.row('posts').nth(0).merge({ content: "This is some replaced content" })
+})
+```
