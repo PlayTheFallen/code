81d38388cafad271aaef3d668bfcba777d2831a0
PlayTheFallen <PlayTheFallen@gmail.com>
2019-04-19 22:10:38 +0100

    missing bracket

--- javascript/Libraries/rethinkdb/edit-array-element.js
+++ javascript/Libraries/rethinkdb/edit-array-element.js
@@ -5,5 +5,5 @@
 
 // not recommended, used only to target a random post within the table
 r.table('posts').filter({author: 1}).limit(1).update({
-  posts: r.row('posts').changeAt(0, r.row('posts').nth(0).merge({ content: "This is some replaced content" })
+  posts: r.row('posts').changeAt(0, r.row('posts').nth(0).merge({ content: "This is some replaced content" }))
 })
